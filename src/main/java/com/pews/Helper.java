b775fca147e08a4d5ee1a82a6d39459316be744a
Pews <billlee978@foxmail.com>
2023-06-15 14:49:23 +0800

    Newer version of System.

--- src/main/java/com/pews/Helper.java
+++ src/main/java/com/pews/Helper.java
@@ -1,7 +1,27 @@
 package com.pews;
 
 public class Helper {
-    public String healHelper(double weight, double height){
-        return "Your BMI is: " + (weight / (height * height));
+    public void healHelper(double weight, double height){
+        double BMI =  (weight / (height * height));
+        String r = "";
+
+        //根据 bmi 指数范围，来给r重新赋值
+        if (BMI < 18.5) {
+            r = "过轻";
+        } else if (BMI <= 22.9) {
+            r = "正常";
+        } else if (BMI <= 24.9) {
+            r = "偏胖";
+        } else if (BMI <= 29.9) {
+            r = "肥胖";
+        } else if (BMI <= 40) {
+            r = "重度肥胖";
+        } else {
+            r = "极度肥胖";
+        }
+        //打印最终结果
+        System.out.println("您的BMI指数：" + BMI);
+
+        System.out.println("您的体重属于：" + r);
     }
 }
--- src/main/java/com/pews/Main.java
+++ src/main/java/com/pews/Main.java
@@ -6,22 +6,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Helper helper = new Helper();
-        System.out.println("请输入你要使用的功能: ");
-        System.out.println("1. 个人健康检测");
-        System.out.println("2. 个人收支记录");
-        System.out.println("3. 个人行程记录");
-        System.out.println("4. 个人生活记录");
-        Scanner scanner = new Scanner(System.in);
-        int command = scanner.nextInt();
-        switch (command) {
-            case 1:
-                System.out.print("请输入你的体重：");
-                double weight = scanner.nextDouble();
-                System.out.print("请输入你的身高：");
-                double height = scanner.nextDouble();
-                System.out.println(helper.healHelper(weight, height));
-            default:
-                System.out.println("错误输入!");
+        int command = 0;
+        while (command != -1) {
+            System.out.println("请输入你要使用的功能: ");
+            System.out.println("1. 个人健康检测");
+            System.out.println("2. 个人收支记录");
+            System.out.println("3. 个人行程记录");
+            System.out.println("4. 个人生活记录");
+            System.out.println("-1. 退出系统");
+            Scanner scanner = new Scanner(System.in);
+            command = scanner.nextInt();
+            switch (command) {
+                case 1:
+                    System.out.print("请输入你的体重(以kg为单位)：");
+                    double weight = scanner.nextDouble();
+                    System.out.print("请输入你的身高(以m为单位)：");
+                    double height = scanner.nextDouble();
+                    helper.healHelper(weight, height);
+                    break;
+                case -1:
+                    System.out.println("感谢使用，祝您生活愉快～");
+                    break;
+                default:
+                    System.out.println("错误输入!");
+            }
         }
     }
 }
